e32306416e207e3825530a31b03604ae200307e3
IRyeong <dasliebeich7@gmail.com>
2023-03-19 17:25:36 +0900

    :sparkles: connect openai api

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
+import openai
+
+openai.api_key = os.getenv("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")
+openai.Model.retrieve("text-davinchi-003")
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, My First Flask!'
+
+@app.route('/', methods='POST')
+def give_answer():
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Say this is a test",
+        max_tokens=7,
+        temperature=0
+    )
+    return completion
