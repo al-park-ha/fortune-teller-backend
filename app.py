1163e9bbc83ae36a19c824666e862de0232afdf9
IRyeong <dasliebeich7@gmail.com>
2023-03-19 17:33:58 +0900

    :bug: connect openai api and check request

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ openai.api_key = os.getenv("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"
 openai.Model.retrieve("text-davinchi-003")
 
 app = Flask(__name__)
+openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Say this is a test",
+    max_tokens=7,
+    temperature=0
+)
 
 
 @app.route('/', methods='POST')
 def give_answer():
-    completion = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Say this is a test",
-        max_tokens=7,
-        temperature=0
-    )
-    return completion
+    gender = request.form['gender']
+    mbti = request.form['mbti']
+    birth = request.form['birth']
+    status_type = request.form['status_type']
+    detail_type = request.form['detail_type']
+    return gender + ' ' + mbti + ' ' + birth + ' ' + status_type + ' ' + detail_type
